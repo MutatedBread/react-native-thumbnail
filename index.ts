d80af92b87b47e20cd65702adbda6947c48b1c08
mutatedbread <ngchenhon@gmail.com>
2018-07-05 18:27:26 +0800

    added typings

---
+++ index.ts
@@ -0,0 +1,61 @@
+
+import { NativeModules, Platform, PermissionsAndroid } from 'react-native';
+
+export type IsMoreThanKitkatCallback = (isKitKat : boolean) => void; 
+
+export interface Thumbnail {
+	width: number,
+	height: number,
+	path: string,
+}
+
+export interface RNThumbnail {
+	get(filePath : string): Thumbnail;
+	isMoreThanKitkat(callback : IsMoreThanKitkatCallback): void;
+}
+
+export interface PermissionHelper {
+	hasPermissions() : boolean;
+	requestPermissions() : boolean;
+	isMoreThanKitkat: IsMoreThanKitkatCallback
+}
+
+const { RNThumbnail } : { RNThumbnail : RNThumbnail } = NativeModules;
+
+const hasPermissionWriteExternal = async () => {
+	return await PermissionsAndroid.check(
+		PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+	);
+};
+
+const hasPermissionReadExternal = async () => {
+	return await PermissionsAndroid.check(
+		PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
+	);
+};
+
+const hasPermissions = async () => {
+	return (
+		(await hasPermissionWriteExternal()) && (await hasPermissionReadExternal())
+	);
+};
+
+const requestPermissions = async () => {
+	try {
+		const granted = await PermissionsAndroid.requestMultiple([
+			PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+			PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
+        ]);
+		return Object.values(granted).every((item : string) => item === "granted");
+	} catch (err) {
+		return false;
+	}
+};
+
+export const PermissionHelper = {
+  hasPermissions,
+  requestPermissions,
+  isMoreThanKitkat: RNThumbnail.isMoreThanKitkat
+};
+
+export default RNThumbnail as RNThumbnail;
